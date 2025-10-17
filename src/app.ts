128984c24ca8ae05daedc65fc508b3636dbceb0f
unknown <aldmyatyfrhh@gmail.com>
2025-10-17 05:30:05 +0300

    create table user , register

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import e from "express";
 import cookieParser from "cookie-parser";
+import authRoutes from './Router/register.router'
 const app = e();
 
 // Middlewares
@@ -7,6 +8,8 @@ app.use(e.json());
 app.use(cookieParser());
 
 // Routes
+
+app.use('/v1/api/',authRoutes)
 
 // Error Handler
 
